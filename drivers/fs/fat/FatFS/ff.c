2ee4e121e56d05ecb1cbe9b06945a34083f38b8e
Eyal Birger <eyal.birger@gmail.com>
2013-08-22 05:35:39 +0300

    [CI] silence clang error by removing useless self assignment

--- drivers/fs/fat/FatFS/ff.c
+++ drivers/fs/fat/FatFS/ff.c
@@ -1142,7 +1142,7 @@ FRESULT dir_next (	/* FR_OK:Succeeded, FR_NO_FILE:End of table, FR_DENIED:Could 
 	WORD i;
 
 
-	stretch = stretch;		/* To suppress warning on read-only cfg. */
+//	stretch = stretch;		/* To suppress warning on read-only cfg. */
 	i = dj->index + 1;
 	if (!i || !dj->sect)	/* Report EOT when index has reached 65535 */
 		return FR_NO_FILE;
