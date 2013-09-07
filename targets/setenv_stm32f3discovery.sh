ddfb44dffef42d6fde873f255b651d55ad49c545
Eyal Birger <eyal.birger@gmail.com>
2013-09-07 07:51:15 +0300

    [STM32] move stm32 f3 dir to tinkerpal

--- targets/setenv_stm32f3discovery.sh
+++ targets/setenv_stm32f3discovery.sh
@@ -13,7 +13,7 @@ elif [[ $CONFIG_GCC == "y" ]] ; then
 
     TOOLCHAIN_DIR=/usr/local/tinkerpal/sat
     
-    export BSP_DIR=/usr/local/stm32/STM32F3-Discovery_FW_V1.1.0
+    export BSP_DIR=/usr/local/tinkerpal/stm32_f3
     export LIBC_DIR=$TOOLCHAIN_DIR/arm-none-eabi/lib/thumb/cortex-m4
     export LIBGCC_DIR=$TOOLCHAIN_DIR/lib/gcc/arm-none-eabi/4.7.3/thumb/cortex-m4
     export CROSS_COMPILE=$TOOLCHAIN_DIR/bin/arm-none-eabi-
