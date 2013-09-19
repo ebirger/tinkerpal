756e33f361bcc65197a60e902ac9334288e0bfc5
Eyal Birger <eyal.birger@gmail.com>
2013-09-19 20:05:00 +0200

    [GCC] when generating .ld files, generate the appropriate .d file and include it

--- scripts/gcc.mk
+++ scripts/gcc.mk
@@ -14,7 +14,8 @@ CFLAGS+=-I. -I$(BUILD) -include $(BUILD)/autoconf.h -Wall -Werror -g
 # Build commands
 compile=$(CC) $(CFLAGS) $(MK_CFLAGS_$@) -c -o $@ $<
 calc_deps=$(CC) $(CFLAGS) -MM  -MT $@ -MF $(@:.o=.d) $<
-include_deps=$(eval -include $(OBJS:.o=.d))
+include_deps=$(eval -include $(OBJS:.o=.d)) \
+  $(if $(LINKER_SCRIPT),$(eval -include $(LINKER_SCRIPT:.ld=.ld.d)))
 link=$(LD) $(LDFLAGS) $(if $(LINKER_SCRIPT),-T $(LINKER_SCRIPT)) -o $@ $(OBJS) $(LIBS)
 obj_to_bin=$(OBJCOPY) -O binary $< $@
 
@@ -25,4 +26,5 @@ IMAGE_NAME:=tp.bin
 # Rules
 $(BUILD)/%.ld : %.ld.S
 	@echo GEN $@
+	$(Q)cpp -MM -MT $@ -MF $(@:.ld=.ld.d) -I. -P $< $@
 	$(Q)cpp -I. -P $< $@
