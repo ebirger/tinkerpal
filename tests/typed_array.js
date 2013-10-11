3f187c6f2dc61dcb5bf87c3b44d19d975bd28a22
Eyal Birger <eyal.birger@gmail.com>
2013-10-11 09:54:12 +0200

    [typed arrays] more tests

--- tests/typed_array.js
+++ tests/typed_array.js
@@ -1,8 +1,38 @@
+/* Assuming little endian */
 var a = new ArrayBuffer(16);
 debug.assert(a.byteLength, 16);
 var k = new Int8Array(a);
-var k2 = new Int16Array(a);
+var k16 = new Int16Array(a);
 k[3] = 5;
 k[3] += 8;
 debug.assert(k[3], 13);
-debug.assert(k2[1], 3328); /* Assuming little endian */
+debug.assert(k16[1], 3328);
+
+var a = new ArrayBuffer(16);
+var k32 = new Int32Array(a);
+
+for (var i=0; i<k32.length; i++) 
+{
+  k32[i] = i*2;
+}
+
+var k16 = new Int16Array(a);
+
+debug.assert(k16[0],0);
+debug.assert(k16[1],0);
+debug.assert(k16[2],2);
+debug.assert(k16[3],0);
+debug.assert(k16[4],4);
+debug.assert(k16[5],0);
+debug.assert(k16[6],6);
+debug.assert(k16[7],0);
+k16[0] = 32;
+debug.assert(k32[0], 32);
+
+var k = new Int8Array(new ArrayBuffer(4));
+
+for (var i = 0; i < 4; i++)
+    k[i] = i;
+var arr = [].map.call(k, function(x) { return x; });
+debug.assert(arr.length, 4);
+debug.assert(arr[3], 3);
