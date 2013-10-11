5ccaae873a76f80c3f4898e22a60baefba1d2522
Eyal Birger <eyal.birger@gmail.com>
2013-10-11 07:57:23 +0200

    [typed arrays] additional test

--- tests/typed_array.js
+++ tests/typed_array.js
@@ -1,6 +1,8 @@
 var a = new ArrayBuffer(16);
 debug.assert(a.byteLength, 16);
 var k = new Int8Array(a);
+var k2 = new Int16Array(a);
 k[3] = 5;
 k[3] += 8;
 debug.assert(k[3], 13);
+debug.assert(k2[1], 3328); /* Assuming little endian */
