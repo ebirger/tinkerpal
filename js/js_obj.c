fdb893add55ba778c169ece1d3f2128117732034
Eyal Birger <eyal.birger@gmail.com>
2013-10-14 22:56:05 +0200

    [reduce libc dependency] remove unneeded includes

--- js/js_obj.c
+++ js/js_obj.c
@@ -22,8 +22,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-#include <stdlib.h>
-#include <string.h>
 #include "util/tp_types.h"
 #include "util/tmalloc.h"
 #include "util/tprintf.h"
