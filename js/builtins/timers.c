6a0207e8c6697d6cfce372ae325ac08128b12662
Eyal Birger <eyal.birger@gmail.com>
2013-12-01 05:47:58 +0200

    [gc preps] add timers registry

--- js/builtins/timers.c
+++ js/builtins/timers.c
@@ -28,6 +28,7 @@
 #include "js/js_obj.h"
 #include "platform/platform.h"
 
+#define Stimers S("timers")
 #define Stimer_func S("timer_func")
 #define Stimer_this S("timer_this")
 #define Stimer_id S("timer_id")
@@ -36,6 +37,38 @@ typedef struct {
     event_timer_t et; /* Must be first */
     obj_t *timer_obj;
 } delayed_work_t;
+
+extern obj_t *meta_env;
+
+void timers_uninit(void);
+void timers_init(void);
+
+static void timer_register(obj_t *timer_obj, int id)
+{
+    obj_t *timers;
+
+    timers = obj_get_property(NULL, meta_env, &Stimers);
+    obj_set_int_property(timers, id, timer_obj);
+    obj_put(timers);
+}
+
+static void timer_unregister_all(void)
+{
+    timers_uninit();
+    timers_init();
+}
+
+static void timer_unregister(int id)
+{
+    obj_t *timers;
+
+    timers = obj_get_property(NULL, meta_env, &Stimers);
+    /* XXX: should be deleting the entry entirely, not just
+     * setting it to undefined
+    */
+    obj_set_int_property(timers, id, UNDEF);
+    obj_put(timers);
+}
 
 static void delayed_work_free(event_timer_t *et)
 {
@@ -77,9 +110,14 @@ static void timeout_cb(event_timer_t *et)
 {
     delayed_work_t *w = (delayed_work_t *)et;
     obj_t *o;
+    int tid;
 
     timer_function_call(&o, w->timer_obj);
 
+    if (obj_get_property_int(&tid, w->timer_obj, &Stimer_id))
+	tp_err(("Weird, no timer id on timer obj\n"));
+    else
+	timer_unregister(tid);
     obj_put(o);
     delayed_work_free(et);
 }
@@ -96,7 +134,10 @@ static void interval_cb(event_timer_t *et)
 	if (obj_get_property_int(&tid, w->timer_obj, &Stimer_id))
 	    tp_warn(("Weird, cannot find timer id\n"));
 	else
+	{
 	    event_timer_del(tid);
+	    timer_unregister(tid);
+	}
 	delayed_work_free(et);
     }
 
@@ -116,6 +157,8 @@ int do_set_timeout(obj_t **ret, obj_t *this, int argc, obj_t *argv[])
 
     tid = event_timer_set(ms, &w->et);
     *ret = num_new_int(tid);
+    obj_set_property(w->timer_obj, Stimer_id, *ret);
+    timer_register(w->timer_obj, tid);
     return 0;
 }
   
@@ -133,6 +176,7 @@ int do_set_interval(obj_t **ret, obj_t *this, int argc, obj_t *argv[])
     tid = event_timer_set_period(ms, &w->et);
     *ret = num_new_int(tid);
     obj_set_property(w->timer_obj, Stimer_id, *ret);
+    timer_register(w->timer_obj, tid);
     return 0;
 }
 
@@ -141,12 +185,16 @@ static int do_clear_timer(obj_t **ret, obj_t *this, int argc, obj_t *argv[])
     tp_assert(argc == 1 || argc == 2);
 
     if (argc == 1)
+    {
 	event_timer_del_all();
+	timer_unregister_all();
+    }
     else
     {
 	int id = NUM_INT(to_num(argv[1]));
 
 	event_timer_del(id);
+	timer_unregister(id);
     }
     return 0;
 }
@@ -168,4 +216,21 @@ int do_get_time(obj_t **ret, obj_t *this, int argc, obj_t *argv[])
     /* Ticking every ms */
     *ret = num_new_fp((double)ticks / 1000);
     return 0;
+}
+
+int do_list_timers(obj_t **ret, obj_t *this, int argc, obj_t *argv[])
+{
+    obj_t *timers = obj_get_property(NULL, meta_env, &Stimers);
+
+    tp_out(("Timer list:\n%o\n", timers));
+    return 0;
+}
+
+void timers_uninit(void)
+{
+}
+
+void timers_init(void)
+{
+    obj_set_property(meta_env, Stimers, array_new());
 }
