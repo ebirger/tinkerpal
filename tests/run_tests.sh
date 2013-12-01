3a515ec6efc48c6a1c68226d555b2bc1fe41a131
Eyal Birger <eyal.birger@gmail.com>
2013-12-01 15:52:24 +0200

    [event] improve timer test & add to test list

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,7 +4,7 @@ if [ $SKIP_TESTS ]; then
     exit 0;
 fi
 
-list="closure_test.js while_test.js func_test.js exp_test.js object_test.js string_test.js prototype_test.js member_test.js for_test.js array_test.js fp_test.js self_ref.js eval_test.js func_constructor_test.js throw_test.js switch_test.js properties_test.js typed_array.js func_bind_test.js func_apply_test.js";
+list="closure_test.js while_test.js func_test.js exp_test.js object_test.js string_test.js prototype_test.js member_test.js for_test.js array_test.js fp_test.js self_ref.js eval_test.js func_constructor_test.js throw_test.js switch_test.js properties_test.js typed_array.js func_bind_test.js func_apply_test.js timer_test.js";
 
 HOST_OS=`uname`;
 
--- tests/timer_test.js
+++ tests/timer_test.js
@@ -1,1 +1,3 @@
-setTimeout(function() { debug.dump("Called"); }, 300);
+var did = 0;
+setTimeout(function() { debug.dump("Called"); did=1; }, 300);
+setTimeout(function() { debug.assert(did, 1); }, 600);
