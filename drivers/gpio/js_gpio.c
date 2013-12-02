a6e292b335959322321d698c46717fb9186d4e02
Eyal Birger <eyal.birger@gmail.com>
2013-12-02 07:01:39 +0200

    [js event] use events registry instead of timers & watches individual registries

--- drivers/gpio/js_gpio.c
+++ drivers/gpio/js_gpio.c
@@ -198,19 +198,6 @@ static int do_pinmode(obj_t **ret, obj_t *this, int argc, obj_t *argv[])
 }
 #endif
 
-#define Swatches S("watches")
-
-extern obj_t *meta_env;
-
-static void watch_register(obj_t *watch_obj, int id)
-{
-    obj_t *watches;
-
-    watches = obj_get_property(NULL, meta_env, &Swatches);
-    obj_set_int_property(watches, id, watch_obj);
-    obj_put(watches);
-}
-
 static void set_watch_on_change_cb(event_t *e, int id)
 {
     obj_t *o, *this, *func;
@@ -236,25 +223,5 @@ int do_set_watch(obj_t **ret, obj_t *this, int argc, obj_t *argv[])
 
     event_id = event_watch_set(obj_get_int(argv[2]), e);
     *ret = num_new_int(event_id);
-    watch_register(js_event_obj(e), event_id);
     return 0;
-}
-
-int do_list_watches(obj_t **ret, obj_t *this, int argc, obj_t *argv[])
-{
-    obj_t *watches = obj_get_property(NULL, meta_env, &Swatches);
-
-    tp_out(("Watch list:\n%o\n", watches));
-    
-    obj_put(watches);
-    return 0;
-}
-
-void js_gpio_uninit(void)
-{
-}
-
-void js_gpio_init(void)
-{
-    _obj_set_property(meta_env, Swatches, array_new());
 }
--- js/builtins/timers.c
+++ js/builtins/timers.c
@@ -29,59 +29,7 @@
 #include "js/js_event.h"
 #include "platform/platform.h"
 
-#define Stimers S("timers")
 #define Stimer_id S("timer_id")
-
-extern obj_t *meta_env;
-
-void timers_uninit(void);
-void timers_init(void);
-
-static void timer_register(obj_t *timer_obj, int id)
-{
-    obj_t *timers;
-
-    timers = obj_get_property(NULL, meta_env, &Stimers);
-    array_push(timers, timer_obj);
-    obj_put(timers);
-}
-
-static void timer_unregister_all(void)
-{
-    timers_uninit();
-    timers_init();
-}
-
-static void timer_unregister(int id)
-{
-    obj_t *timers;
-    array_iter_t iter;
-    int idx = -1;
-
-    timers = obj_get_property(NULL, meta_env, &Stimers);
-
-    /* Lookup our timer */
-    array_iter_init(&iter, timers, 0);
-    while (array_iter_next(&iter))
-    {
-	int tid;
-
-	if (obj_get_property_int(&tid, iter.obj, &Stimer_id))
-	    continue;
-
-	if (tid != id)
-	    continue;
-
-	idx = iter.k;
-    }
-    array_iter_uninit(&iter);
-
-    /* XXX: should be deleting the entry entirely, not just
-     * setting it to undefined
-    */
-    obj_set_int_property(timers, idx, UNDEF);
-    obj_put(timers);
-}
 
 static int timer_function_call(obj_t **po, event_t *e)
 {
@@ -101,14 +49,9 @@ static int timer_function_call(obj_t **po, event_t *e)
 static void timeout_cb(event_t *e, int resource_id)
 {
     obj_t *o;
-    int tid;
 
     timer_function_call(&o, e);
 
-    if (obj_get_property_int(&tid, js_event_obj(e), &Stimer_id))
-	tp_err(("Weird, no timer id on timer obj\n"));
-    else
-	timer_unregister(tid);
     obj_put(o);
     js_event_free(e);
 }
@@ -119,16 +62,10 @@ static void interval_cb(event_t *e, int resource_id)
 
     if (timer_function_call(&o, e))
     {
-	int tid;
+	int tid = 0;
 
-	if (obj_get_property_int(&tid, js_event_obj(e), &Stimer_id))
-	    tp_warn(("Weird, cannot find timer id\n"));
-	else
-	{
-	    event_timer_del(tid);
-	    timer_unregister(tid);
-	}
-	js_event_free(e);
+	obj_get_property_int(&tid, js_event_obj(e), &Stimer_id);
+	event_timer_del(tid);
     }
 
     obj_put(o);
@@ -148,7 +85,6 @@ int do_set_timeout(obj_t **ret, obj_t *this, int argc, obj_t *argv[])
     tid = event_timer_set(ms, e);
     *ret = num_new_int(tid);
     obj_set_property(js_event_obj(e), Stimer_id, *ret);
-    timer_register(js_event_obj(e), tid);
     return 0;
 }
   
@@ -166,7 +102,6 @@ int do_set_interval(obj_t **ret, obj_t *this, int argc, obj_t *argv[])
     tid = event_timer_set_period(ms, e);
     *ret = num_new_int(tid);
     obj_set_property(js_event_obj(e), Stimer_id, *ret);
-    timer_register(js_event_obj(e), tid);
     return 0;
 }
 
@@ -175,16 +110,12 @@ static int do_clear_timer(obj_t **ret, obj_t *this, int argc, obj_t *argv[])
     tp_assert(argc == 1 || argc == 2);
 
     if (argc == 1)
-    {
 	event_timer_del_all();
-	timer_unregister_all();
-    }
     else
     {
 	int id = NUM_INT(to_num(argv[1]));
 
 	event_timer_del(id);
-	timer_unregister(id);
     }
     return 0;
 }
@@ -206,23 +137,4 @@ int do_get_time(obj_t **ret, obj_t *this, int argc, obj_t *argv[])
     /* Ticking every ms */
     *ret = num_new_fp((double)ticks / 1000);
     return 0;
-}
-
-int do_list_timers(obj_t **ret, obj_t *this, int argc, obj_t *argv[])
-{
-    obj_t *timers = obj_get_property(NULL, meta_env, &Stimers);
-
-    tp_out(("Timer list:\n%o\n", timers));
-
-    obj_put(timers);
-    return 0;
-}
-
-void timers_uninit(void)
-{
-}
-
-void timers_init(void)
-{
-    _obj_set_property(meta_env, Stimers, array_new());
 }
