fccaea18cce6332059834d7385585de4b6997274
Eyal Birger <eyal.birger@gmail.com>
2013-12-20 22:00:43 +0200

    [net] js binding for linux packet Ethernet driver

---
+++ drivers/net/js_linux_packet_eth.c
@@ -0,0 +1,48 @@
+/* Copyright (c) 2013, Eyal Birger
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * The name of the author may not be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "js/js_obj.h"
+#include "js/js_utils.h"
+#include "js/js_event.h"
+#include "net/js_etherif.h"
+#include "drivers/net/linux_packet_eth.h"
+
+int do_linux_packet_eth_constructor(obj_t **ret, obj_t *this, int argc,
+    obj_t *argv[])
+{
+    tstr_t dev_name_tstr;
+    char *dev_name;
+    etherif_t *ethif;
+
+    if (argc != 2)
+	return js_invalid_args(ret);
+
+    dev_name_tstr = obj_get_str(argv[1]);
+    dev_name = tstr_to_strz(&dev_name_tstr);
+    ethif = linux_packet_eth_new(dev_name);
+    tstr_free(&dev_name_tstr);
+    tfree(dev_name);
+
+    return etherif_obj_constructor(ethif, ret, this, argc, argv);
+}
