586525ce936509cca0a84f6b6c75c06b9073bb01
Eyal Birger <eyal.birger@gmail.com>
2013-12-22 06:32:58 +0200

    [net] stellaris eth: initialization and phy status

--- platform/arm/stellaris/stellaris_eth.c
+++ platform/arm/stellaris/stellaris_eth.c
@@ -28,16 +28,24 @@
 #include "util/debug.h"
 #include "util/event.h"
 #include "mem/tmalloc.h"
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_ethernet.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/ethernet.h"
+#include "driverlib/interrupt.h"
 
 typedef struct {
     etherif_t ethif;
 } stellaris_eth_t;
 
-#define ETHIF_TO_STELLARIS_ETH(x) container_of(x, stellaris_eth_t, ethif)
+static stellaris_eth_t g_eth; /* Singleton */
 
 static int stellaris_eth_link_status(etherif_t *ethif)
 {
-    return 1;
+    return MAP_EthernetPHYRead(ETH_BASE, PHY_MR1) & PHY_MR1_LINK ? 1 : 0;
 }
 
 static int stellaris_eth_packet_size(etherif_t *ethif)
@@ -56,10 +64,7 @@ static void stellaris_eth_packet_xmit(etherif_t *ethif, u8 *buf, int size)
 
 void stellaris_eth_free(etherif_t *ethif)
 {
-    stellaris_eth_t *se = ETHIF_TO_STELLARIS_ETH(ethif);
-
     etherif_uninit(ethif);
-    tfree(se);
 }
 
 static const etherif_ops_t stellaris_eth_etherif_ops = {
@@ -69,11 +74,54 @@ static const etherif_ops_t stellaris_eth_etherif_ops = {
     .packet_xmit = stellaris_eth_packet_xmit,
 };
 
+static void phy_info(void)
+{
+    u32 mr2, mr3;
+    
+    mr2 = MAP_EthernetPHYRead(ETH_BASE, PHY_MR2);
+    mr3 = MAP_EthernetPHYRead(ETH_BASE, PHY_MR3);
+    tp_out(("PHY OUI %x:%x\n", (mr2 & PHY_MR2_OUI_M) >> PHY_MR2_OUI_S,
+	(mr3 & PHY_MR3_OUI_M) >> PHY_MR3_OUI_S));
+    tp_out(("Model Number %x\n", (mr3 & PHY_MR3_MN_M) >> PHY_MR3_MN_S));
+    tp_out(("Revision Number %x\n", (mr3 & PHY_MR3_RN_M) >> PHY_MR3_RN_S));
+}
+
+static void hw_init(void)
+{
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_ETH);
+    MAP_SysCtlPeripheralReset(SYSCTL_PERIPH_ETH);
+
+    /* Disable all Ethernet Interrupts. */
+    MAP_EthernetIntDisable(ETH_BASE, ETH_INT_PHY | ETH_INT_MDIO |
+	ETH_INT_RXER | ETH_INT_RXOF | ETH_INT_TX | ETH_INT_TXER | ETH_INT_RX);
+    /* Disable any possible pending interrupts */
+    MAP_EthernetIntClear(ETH_BASE, MAP_EthernetIntStatus(ETH_BASE, false));
+
+    /* Initialize the Ethernet Controller. */
+    MAP_EthernetInitExpClk(ETH_BASE, MAP_SysCtlClockGet());
+
+    /* Configure the Ethernet Controller */
+    MAP_EthernetConfigSet(ETH_BASE, ETH_CFG_TX_DPLXEN | ETH_CFG_TX_CRCEN |
+	ETH_CFG_TX_PADEN | ETH_CFG_RX_AMULEN);
+
+    /* Enable the Ethernet Controller transmitter and receiver */
+    MAP_EthernetEnable(ETH_BASE);
+
+    /* Enable the Ethernet Interrupt handler */
+    MAP_IntEnable(INT_ETH);
+
+    /* Enable Ethernet TX and RX Packet Interrupts */
+    MAP_EthernetIntEnable(ETH_BASE, ETH_INT_RX | ETH_INT_TX);
+}
+
 etherif_t *stellaris_eth_new(void)
 {
-    stellaris_eth_t *se = tmalloc_type(stellaris_eth_t);
+    stellaris_eth_t *se = &g_eth;
 
     etherif_init(&se->ethif, &stellaris_eth_etherif_ops);
+
+    hw_init();
+    phy_info();
 
     return &se->ethif;
 }
