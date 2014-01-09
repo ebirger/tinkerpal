a78be9b3438948c3b02c74b6cd7a351ee561c075
Eyal Birger <eyal.birger@gmail.com>
2014-01-09 22:45:13 +0200

    [graphics] change print color

--- apps/graphics_test.c
+++ apps/graphics_test.c
@@ -63,7 +63,7 @@ static void graphics_test_process_line(tstr_t *line)
 	    canvas->width / 4);
     }
     if (!tstr_cmp(line, &S("text")))
-	string_draw(canvas, 10, 10, &S("Hello TinkerPal"), 50);
+	string_draw(canvas, 10, 10, &S("Hello TinkerPal"), 0xffff);
 
     console_printf("Ok\n");
 }
