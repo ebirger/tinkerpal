1ea45969571f2c3e02f69fba9eee76a0f769cde3
Eyal Birger <eyal.birger@gmail.com>
2014-01-12 22:18:40 +0200

    [boards] add mmc hookup information to lm3s6965 board info

--- boards/board_lm3s6965.c
+++ boards/board_lm3s6965.c
@@ -28,4 +28,11 @@
 const board_t board = {
     .desc = "EK LM3S6965",
     .default_console_id = UART_RES(UART0),
+#ifdef CONFIG_MMC
+    .mmc_params = {
+	.spi_port = SPI_RES(SSI0),
+	.mosi = GPIO_RES(PA5),
+	.cs = GPIO_RES(PD0),
+    },
+#endif
 };
