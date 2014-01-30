12e7b1b7b36197900768203f3f107bb6bc03622b
Eyal Birger <eyal.birger@gmail.com>
2014-01-30 21:39:51 +0200

    [stm32] add armjishu 28" board support

---
+++ boards/env/setenv_stm32_armjishu_28.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+host_os=`uname`;
+
+source build.$host_os/.config
+
+if [[ $CONFIG_STM32_ARMJISHU_28 != "y" ]] ; then
+
+    echo "Target configuration is not for the CONFIG_STM32_ARMJISHU_28 board";
+    echo "Please run 'make stm32_armjishu_28_gcc_defconfig'";
+
+elif [[ $CONFIG_GCC == "y" ]] ; then
+
+    TOOLCHAIN_DIR=/usr/local/tinkerpal/sat
+    
+    export BSP_DIR=/usr/local/tinkerpal/stm32_f1
+    export LIBC_DIR=$TOOLCHAIN_DIR/arm-none-eabi/lib/thumb/cortex-m3
+    export CROSS_COMPILE=$TOOLCHAIN_DIR/bin/arm-none-eabi-
+
+else
+
+    echo "No support for non GCC builds on this platform yet";
+
+fi
