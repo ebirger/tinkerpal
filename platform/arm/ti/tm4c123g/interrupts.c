c5c7c8fe4cc376b060833f9cd0cab39a2dc2ebce
Eyal Birger <eyal.birger@gmail.com>
2014-03-21 12:07:17 +0200

    [tiva c] initial tiva c launchpad support. Based on Stellaris launchpad

---
+++ platform/arm/ti/tm4c123g/interrupts.c
@@ -0,0 +1,238 @@
+/* Copyright (c) 2013, Eyal Birger
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * The name of the author may not be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "inc/hw_nvic.h"
+#include "inc/hw_types.h"
+#include "platform/arm/ti/tm4c123g/tm4c123g.h"
+
+extern void reset_isr(void);
+extern void ti_arm_mcu_uart_isr(int u);
+void ti_arm_mcu_gpio_isr(int port) { }
+extern void cortex_m_systick_isr(void);
+
+static void uart0_isr(void) { ti_arm_mcu_uart_isr(UART0); }
+static void uart1_isr(void) { ti_arm_mcu_uart_isr(UART1); }
+static void uart2_isr(void) { ti_arm_mcu_uart_isr(UART2); }
+static void uart3_isr(void) { ti_arm_mcu_uart_isr(UART3); }
+static void uart4_isr(void) { ti_arm_mcu_uart_isr(UART4); }
+static void uart5_isr(void) { ti_arm_mcu_uart_isr(UART5); }
+static void uart6_isr(void) { ti_arm_mcu_uart_isr(UART6); }
+static void uart7_isr(void) { ti_arm_mcu_uart_isr(UART7); }
+
+static void gpio_port_a_isr(void) { ti_arm_mcu_gpio_isr(GPIO_PORT_A); }
+static void gpio_port_b_isr(void) { ti_arm_mcu_gpio_isr(GPIO_PORT_B); }
+static void gpio_port_c_isr(void) { ti_arm_mcu_gpio_isr(GPIO_PORT_C); }
+static void gpio_port_d_isr(void) { ti_arm_mcu_gpio_isr(GPIO_PORT_D); }
+static void gpio_port_e_isr(void) { ti_arm_mcu_gpio_isr(GPIO_PORT_E); }
+static void gpio_port_f_isr(void) { ti_arm_mcu_gpio_isr(GPIO_PORT_F); }
+
+static void nmi_isr(void)
+{
+    /* Hang in there doing nothing */
+    while(1);
+}
+
+static void fault_isr(void)
+{
+    /* Hang in there doing nothing */
+    while(1);
+}
+
+static void default_isr(void)
+{
+    /* Hang in there doing nothing */
+    while(1);
+}
+
+/* Linker variable that marks the top of the stack */
+extern unsigned long _stack_top;
+
+/* The vector table.  Note that the proper constructs must be placed on this to
+ * ensure that it ends up at physical address 0x0000.0000.
+ */
+#ifdef CONFIG_GCC
+__attribute__ ((section(".isr_vector")))
+#elif defined(CONFIG_TI_CCS5)
+#pragma DATA_SECTION(g_pfnVectors, ".intvecs")
+#else
+#error Compilation environment not set
+#endif
+void (*const g_pfnVectors[])(void) =
+{
+    (void (*)(void))((unsigned long) &_stack_top), // The initial stack pointer
+    reset_isr,                        // The reset handler
+    nmi_isr,                          // The NMI handler
+    fault_isr,                        // The hard fault handler
+    default_isr,                      // The MPU fault handler
+    default_isr,                      // The bus fault handler
+    default_isr,                      // The usage fault handler
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    default_isr,                      // SVCall handler
+    default_isr,                      // Debug monitor handler
+    0,                                // Reserved
+    default_isr,                      // The PendSV handler
+    cortex_m_systick_isr,             // The SysTick handler
+    gpio_port_a_isr,                  // GPIO Port A
+    gpio_port_b_isr,                  // GPIO Port B
+    gpio_port_c_isr,                  // GPIO Port C
+    gpio_port_d_isr,                  // GPIO Port D
+    gpio_port_e_isr,                  // GPIO Port E
+    uart0_isr,	                      // UART0 Rx and Tx
+    uart1_isr,                        // UART1 Rx and Tx
+    default_isr,                      // SSI0 Rx and Tx
+    default_isr,                      // I2C0 Master and Slave
+    default_isr,                      // PWM Fault
+    default_isr,                      // PWM Generator 0
+    default_isr,                      // PWM Generator 1
+    default_isr,                      // PWM Generator 2
+    default_isr,                      // Quadrature Encoder 0
+    default_isr,                      // ADC Sequence 0
+    default_isr,                      // ADC Sequence 1
+    default_isr,                      // ADC Sequence 2
+    default_isr,                      // ADC Sequence 3
+    default_isr,                      // Watchdog timer
+    default_isr,                      // Timer 0 subtimer A
+    default_isr,                      // Timer 0 subtimer B
+    default_isr,                      // Timer 1 subtimer A
+    default_isr,                      // Timer 1 subtimer B
+    default_isr,                      // Timer 2 subtimer A
+    default_isr,                      // Timer 2 subtimer B
+    default_isr,                      // Analog Comparator 0
+    default_isr,                      // Analog Comparator 1
+    default_isr,                      // Analog Comparator 2
+    default_isr,                      // System Control (PLL, OSC, BO)
+    default_isr,                      // FLASH Control
+    gpio_port_f_isr,	              // GPIO Port F
+    default_isr,                      // GPIO Port G
+    default_isr,                      // GPIO Port H
+    uart2_isr,                        // UART2 Rx and Tx
+    default_isr,                      // SSI1 Rx and Tx
+    default_isr,                      // Timer 3 subtimer A
+    default_isr,                      // Timer 3 subtimer B
+    default_isr,                      // I2C1 Master and Slave
+    default_isr,                      // Quadrature Encoder 1
+    default_isr,                      // CAN0
+    default_isr,                      // CAN1
+    default_isr,                      // Reserved
+    default_isr,                      // Reserved
+    default_isr,                      // Hibernate
+    default_isr,                      // USB0
+    default_isr,                      // PWM Generator 3
+    default_isr,                      // uDMA Software Transfer
+    default_isr,                      // uDMA Error
+    default_isr,                      // ADC1 Sequence 0
+    default_isr,                      // ADC1 Sequence 1
+    default_isr,                      // ADC1 Sequence 2
+    default_isr,                      // ADC1 Sequence 3
+    default_isr,                      // Reserved
+    default_isr,                      // Reserved
+    default_isr,                      // GPIO Port J
+    default_isr,                      // GPIO Port K
+    default_isr,                      // GPIO Port L
+    default_isr,                      // SSI2 Rx and Tx
+    default_isr,                      // SSI3 Rx and Tx
+    uart3_isr,                        // UART3 Rx and Tx
+    uart4_isr,                        // UART4 Rx and Tx
+    uart5_isr,                        // UART5 Rx and Tx
+    uart6_isr,                        // UART6 Rx and Tx
+    uart7_isr,                        // UART7 Rx and Tx
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    default_isr,                      // I2C2 Master and Slave
+    default_isr,                      // I2C3 Master and Slave
+    default_isr,                      // Timer 4 subtimer A
+    default_isr,                      // Timer 4 subtimer B
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    0,                                // Reserved
+    default_isr,                      // Timer 5 subtimer A
+    default_isr,                      // Timer 5 subtimer B
+    default_isr,                      // Wide Timer 0 subtimer A
+    default_isr,                      // Wide Timer 0 subtimer B
+    default_isr,                      // Wide Timer 1 subtimer A
+    default_isr,                      // Wide Timer 1 subtimer B
+    default_isr,                      // Wide Timer 2 subtimer A
+    default_isr,                      // Wide Timer 2 subtimer B
+    default_isr,                      // Wide Timer 3 subtimer A
+    default_isr,                      // Wide Timer 3 subtimer B
+    default_isr,                      // Wide Timer 4 subtimer A
+    default_isr,                      // Wide Timer 4 subtimer B
+    default_isr,                      // Wide Timer 5 subtimer A
+    default_isr,                      // Wide Timer 5 subtimer B
+    default_isr,                      // FPU
+    default_isr,                      // Reserved
+    default_isr,                      // Reserved
+    default_isr,                      // I2C4 Master and Slave
+    default_isr,                      // I2C5 Master and Slave
+    default_isr,                      // GPIO Port M
+    default_isr,                      // GPIO Port N
+    default_isr,                      // Quadrature Encoder 2
+    default_isr,                      // Reserved
+    0,                                // Reserved
+    default_isr,                      // GPIO Port P (Summary or P0)
+    default_isr,                      // GPIO Port P1
+    default_isr,                      // GPIO Port P2
+    default_isr,                      // GPIO Port P3
+    default_isr,                      // GPIO Port P4
+    default_isr,                      // GPIO Port P5
+    default_isr,                      // GPIO Port P6
+    default_isr,                      // GPIO Port P7
+    default_isr,                      // GPIO Port Q (Summary or Q0)
+    default_isr,                      // GPIO Port Q1
+    default_isr,                      // GPIO Port Q2
+    default_isr,                      // GPIO Port Q3
+    default_isr,                      // GPIO Port Q4
+    default_isr,                      // GPIO Port Q5
+    default_isr,                      // GPIO Port Q6
+    default_isr,                      // GPIO Port Q7
+    default_isr,                      // GPIO Port R
+    default_isr,                      // GPIO Port S
+    default_isr,                      // PWM 1 Generator 0
+    default_isr,                      // PWM 1 Generator 1
+    default_isr,                      // PWM 1 Generator 2
+    default_isr,                      // PWM 1 Generator 3
+    default_isr                       // PWM 1 Fault
+};
---
+++ platform/arm/ti/tm4c123g/tm4c123g.c
@@ -0,0 +1,289 @@
+/* Copyright (c) 2013, Eyal Birger
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * The name of the author may not be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "inc/hw_memmap.h"
+#include "inc/hw_sysctl.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_gpio.h"
+#include "driverlib/debug.h"
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "driverlib/adc.h"
+#include "driverlib/rom_map.h"
+#include "util/debug.h"
+#include "drivers/gpio/gpio_platform.h"
+#include "drivers/serial/serial_platform.h"
+#include "platform/platform.h"
+#include "platform/arm/cortex-m.h"
+#include "platform/arm/ti/ti_arm_mcu.h"
+#include "platform/arm/ti/tm4c123g/tm4c123g.h"
+
+const ti_arm_mcu_gpio_port_t ti_arm_mcu_gpio_ports[] = {
+    [GPIO_PORT_A] = { SYSCTL_PERIPH_GPIOA, GPIO_PORTA_BASE, INT_GPIOA },
+    [GPIO_PORT_B] = { SYSCTL_PERIPH_GPIOB, GPIO_PORTB_BASE, INT_GPIOB },
+    [GPIO_PORT_C] = { SYSCTL_PERIPH_GPIOC, GPIO_PORTC_BASE, INT_GPIOC },
+    [GPIO_PORT_D] = { SYSCTL_PERIPH_GPIOD, GPIO_PORTD_BASE, INT_GPIOD },
+    [GPIO_PORT_E] = { SYSCTL_PERIPH_GPIOE, GPIO_PORTE_BASE, INT_GPIOE },
+    [GPIO_PORT_F] = { SYSCTL_PERIPH_GPIOF, GPIO_PORTF_BASE, INT_GPIOF }
+};
+
+const ti_arm_mcu_uart_t ti_arm_mcu_uarts[] = {
+    [UART0] = { SYSCTL_PERIPH_UART0, UART0_BASE, INT_UART0, PA0, PA1 },
+    [UART1] = { SYSCTL_PERIPH_UART1, UART1_BASE, INT_UART1, PB0, PB1 },
+    [UART2] = { SYSCTL_PERIPH_UART2, UART2_BASE, INT_UART2, PD6, PD7 },
+    [UART3] = { SYSCTL_PERIPH_UART3, UART3_BASE, INT_UART3, PC6, PC7 },
+    [UART4] = { SYSCTL_PERIPH_UART4, UART4_BASE, INT_UART4, PC4, PC5 },
+    [UART5] = { SYSCTL_PERIPH_UART5, UART5_BASE, INT_UART5, PE4, PE5 },
+    [UART6] = { SYSCTL_PERIPH_UART6, UART6_BASE, INT_UART6, PD4, PD5 },
+    [UART7] = { SYSCTL_PERIPH_UART7, UART7_BASE, INT_UART7, PE0, PE1 },
+};
+
+const ti_arm_mcu_ssi_t ti_arm_mcu_ssis[] = {
+    [SSI0] = { .periph = SYSCTL_PERIPH_SSI0, .base = SSI0_BASE, .clk = PA2, .fss = PA3, .rx = PA4, .tx = PA5, },
+    [SSI1] = { .periph = SYSCTL_PERIPH_SSI1, .base = SSI1_BASE, .clk = PD0, .fss = PD1, .rx = PD2, .tx = PD3, },
+};
+
+const ti_arm_mcu_timer_t ti_arm_mcu_timers[] = {
+    [ TIMER0 ] = { SYSCTL_PERIPH_TIMER0, TIMER0_BASE },
+    [ TIMER1 ] = { SYSCTL_PERIPH_TIMER1, TIMER1_BASE },
+    [ TIMER2 ] = { SYSCTL_PERIPH_TIMER2, TIMER2_BASE },
+    [ TIMER3 ] = { SYSCTL_PERIPH_TIMER3, TIMER3_BASE },
+    [ TIMER4 ] = { SYSCTL_PERIPH_TIMER4, TIMER4_BASE },
+    [ TIMER5 ] = { SYSCTL_PERIPH_TIMER5, TIMER5_BASE },
+    [ WTIMER0 ] = { SYSCTL_PERIPH_WTIMER0, WTIMER0_BASE },
+    [ WTIMER1 ] = { SYSCTL_PERIPH_WTIMER1, WTIMER1_BASE },
+    [ WTIMER2 ] = { SYSCTL_PERIPH_WTIMER2, WTIMER2_BASE },
+    [ WTIMER3 ] = { SYSCTL_PERIPH_WTIMER3, WTIMER3_BASE },
+    [ WTIMER4 ] = { SYSCTL_PERIPH_WTIMER4, WTIMER4_BASE },
+    [ WTIMER5 ] = { SYSCTL_PERIPH_WTIMER5, WTIMER5_BASE }
+};
+
+/* Notes:
+ * - PC4/5 can be used for either UART1 or UART4. Currently going for 4, 
+ * since PB0/1 can be used for UART1
+ * - PF timers are shared with PB
+ */
+const ti_arm_mcu_gpio_pin_t ti_arm_mcu_gpio_pins[] = {
+    [ PA0 ] = {-1, 0, -1, GPIO_PA0_U0RX, -1},
+    [ PA1 ] = {-1, 0, -1, GPIO_PA1_U0TX, -1},
+    [ PA2 ] = {-1, 0, -1, -1, GPIO_PA2_SSI0CLK},
+    [ PA3 ] = {-1, 0, -1, -1, GPIO_PA3_SSI0FSS},
+    [ PA4 ] = {-1, 0, -1, -1, GPIO_PA4_SSI0RX},
+    [ PA5 ] = {-1, 0, -1, -1, GPIO_PA5_SSI0TX},
+    [ PA6 ] = {-1, 0, -1, -1, -1},
+    [ PA7 ] = {-1, 0, -1, -1, -1},
+    [ PB0 ] = {TIMER2, GPIO_PB0_T2CCP0, -1, GPIO_PB0_U1RX, -1},
+    [ PB1 ] = {TIMER2, GPIO_PB1_T2CCP1, -1, GPIO_PB1_U1TX, -1},
+    [ PB2 ] = {TIMER3, GPIO_PB2_T3CCP0, -1, -1, -1},
+    [ PB3 ] = {TIMER3, GPIO_PB3_T3CCP1, -1, -1, -1},
+    [ PB4 ] = {TIMER1, GPIO_PB4_T1CCP0, ADC_CTL_CH10, -1, -1},
+    [ PB5 ] = {TIMER1, GPIO_PB5_T1CCP1, ADC_CTL_CH11, -1, -1},
+    [ PB6 ] = {TIMER0, GPIO_PB6_T0CCP0, -1, -1, -1},
+    [ PB7 ] = {TIMER0, GPIO_PB7_T0CCP1, -1, -1, -1},
+    [ PC0 ] = {TIMER4, GPIO_PC0_T4CCP0, -1, -1, -1},
+    [ PC1 ] = {TIMER4, GPIO_PC1_T4CCP1, -1, -1, -1},
+    [ PC2 ] = {TIMER5, GPIO_PC2_T5CCP0, -1, -1, -1},
+    [ PC3 ] = {TIMER5, GPIO_PC3_T5CCP1, -1, -1, -1},
+    [ PC4 ] = {WTIMER0, GPIO_PC4_WT0CCP0, -1, GPIO_PC4_U4RX, -1},
+    [ PC5 ] = {WTIMER0, GPIO_PC5_WT0CCP1, -1, GPIO_PC5_U4TX, -1},
+    [ PC6 ] = {WTIMER1, GPIO_PC6_WT1CCP0, -1, GPIO_PC6_U3RX, -1},
+    [ PC7 ] = {WTIMER1, GPIO_PC7_WT1CCP1, -1, GPIO_PC7_U3TX, -1},
+    [ PD0 ] = {WTIMER2, GPIO_PD0_WT2CCP0, ADC_CTL_CH7, -1, GPIO_PD0_SSI1CLK},
+    [ PD1 ] = {WTIMER2, GPIO_PD1_WT2CCP1, ADC_CTL_CH6, -1, GPIO_PD1_SSI1FSS},
+    [ PD2 ] = {WTIMER3, GPIO_PD2_WT3CCP0, ADC_CTL_CH5, -1, GPIO_PD2_SSI1RX},
+    [ PD3 ] = {WTIMER3, GPIO_PD3_WT3CCP1, ADC_CTL_CH4, -1, GPIO_PD3_SSI1TX},
+    [ PD4 ] = {WTIMER4, GPIO_PD4_WT4CCP0, -1, GPIO_PD4_U6RX, -1},
+    [ PD5 ] = {WTIMER4, GPIO_PD5_WT4CCP1, -1, GPIO_PD5_U6TX, -1},
+    [ PD6 ] = {WTIMER5, GPIO_PD6_WT5CCP0, -1, GPIO_PD6_U2RX, -1},
+    [ PD7 ] = {WTIMER5, GPIO_PD7_WT5CCP1, -1, GPIO_PD7_U2TX, -1},
+    [ PE0 ] = {-1, 0, ADC_CTL_CH3, GPIO_PE0_U7RX, -1},
+    [ PE1 ] = {-1, 0, ADC_CTL_CH2, GPIO_PE1_U7TX, -1},
+    [ PE2 ] = {-1, 0, ADC_CTL_CH1, -1, -1},
+    [ PE3 ] = {-1, 0, ADC_CTL_CH0, -1, -1},
+    [ PE4 ] = {-1, 0, ADC_CTL_CH9, GPIO_PE4_U5RX, -1},
+    [ PE5 ] = {-1, 0, ADC_CTL_CH8, GPIO_PE5_U5TX, -1},
+    [ PE6 ] = {-1, 0, -1, -1, -1},
+    [ PE7 ] = {-1, 0, -1, -1, -1},
+    [ PF0 ] = {TIMER0, GPIO_PF0_T0CCP0, -1, -1, -1},
+    [ PF1 ] = {TIMER0, GPIO_PF1_T0CCP1, -1, -1, -1},
+    [ PF2 ] = {TIMER1, GPIO_PF2_T1CCP0, -1, -1, -1},
+    [ PF3 ] = {TIMER1, GPIO_PF3_T1CCP1, -1, -1, -1},
+    [ PF4 ] = {TIMER2, GPIO_PF4_T2CCP0, -1, -1, -1},
+    [ PF5 ] = {-1, 0, -1, -1, -1},
+    [ PF6 ] = {-1, 0, -1, -1, -1},
+    [ PF7 ] = {-1, 0, -1, -1, -1}
+};
+
+#define HALF_TIMER(p) ((p) & 0x1 ? TIMER_B : TIMER_A) /* even pins use TIMER_A, odd pins use TIMER_B */
+#define TIMER_SET(p, t) ROM_TimerMatchSet(ti_arm_mcu_timers[ti_arm_mcu_gpio_pins[p].timer].base, HALF_TIMER(p), t)
+
+#ifdef CONFIG_GPIO
+
+static void pinmode_pwm(int pin)
+{
+    unsigned long timer, half_timer;
+
+    timer = ti_arm_mcu_timers[ti_arm_mcu_gpio_pins[pin].timer].base;
+    half_timer = HALF_TIMER(pin);
+
+    ti_arm_mcu_periph_enable(ti_arm_mcu_timers[ti_arm_mcu_gpio_pins[pin].timer].periph);
+
+    /* Configure GPIO */
+    ti_arm_mcu_pin_mode_timer(pin);
+
+    /* Configure Timer */
+    ROM_TimerConfigure(timer, (TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PWM | 
+	TIMER_CFG_B_PWM));
+    ROM_TimerPrescaleSet(timer, half_timer, 0); // ~1230 Hz PWM
+    /* Timer will load this value on timeout */
+    ROM_TimerLoadSet(timer, half_timer, 65279);
+    /* Initial duty cycle of 0 */
+    TIMER_SET(pin, 65278);
+    /* PWM should not be inverted */
+    ROM_TimerControlLevel(timer, half_timer, 0);
+    
+    ROM_TimerEnable(timer, half_timer);
+}
+
+static int tm4c123g_set_pin_mode(int pin, gpio_pin_mode_t mode)
+{
+    /* Anti-brick JTAG Protection */
+    if (pin >= PC0 && pin <= PC3) 
+	return -1;
+
+    if (mode == GPIO_PM_OUTPUT_ANALOG && ti_arm_mcu_gpio_pins[pin].timer == -1)
+	return -1;
+
+    ti_arm_mcu_periph_enable(ti_arm_mcu_gpio_periph(pin));
+
+    switch (mode)
+    {
+    case GPIO_PM_INPUT:
+	ti_arm_mcu_gpio_input(pin);
+	ti_arm_mcu_pin_config(pin, GPIO_PIN_TYPE_STD);
+	break;
+    case GPIO_PM_OUTPUT:
+	ti_arm_mcu_pin_mode_output(pin);
+	ti_arm_mcu_pin_config(pin, GPIO_PIN_TYPE_STD);
+	break;
+    case GPIO_PM_INPUT_PULLUP:
+	ti_arm_mcu_gpio_input(pin);
+	ti_arm_mcu_pin_config(pin, GPIO_PIN_TYPE_STD_WPU);
+	break;
+    case GPIO_PM_INPUT_PULLDOWN:
+	ti_arm_mcu_gpio_input(pin);
+	ti_arm_mcu_pin_config(pin, GPIO_PIN_TYPE_STD_WPD);
+	break;
+    case GPIO_PM_INPUT_ANALOG:
+	if (ti_arm_mcu_pin_mode_adc(pin))
+	    return -1;
+	break;
+    case GPIO_PM_OUTPUT_ANALOG:
+	pinmode_pwm(pin);
+	break;
+    }
+    return 0;
+}
+
+static void ti_arm_mcu_gpio_analog_write(int pin, double value) 
+{
+    int int_val;
+
+    int_val = 255 * value;
+
+    if (int_val > 255)
+	int_val = 255;
+    
+    if (int_val == 0)
+	int_val = 65278;
+    else
+	int_val = 65280 - (256 * int_val);
+
+    TIMER_SET(pin, int_val);
+}
+
+#endif
+
+static int ti_arm_mcu_serial_enable(int u, int enabled)
+{
+    ti_arm_mcu_uart_enable(u, enabled);
+    return 0;
+}
+
+static void tm4c123g_init(void)
+{
+    /* Enable lazy stacking for interrupt handlers.  This allows floating-point
+     * instructions to be used within interrupt handlers, but at the expense of
+     * extra stack usage.
+     */
+    ROM_FPULazyStackingEnable();
+
+    /* Set the clocking to run directly from the crystal at 80 MHz */
+    ROM_SysCtlClockSet(SYSCTL_RCC2_DIV400 | SYSCTL_SYSDIV_2_5 |
+	SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
+                       
+    ti_arm_mcu_systick_init();
+}
+
+const platform_t platform = {
+    .serial = {
+	.enable = ti_arm_mcu_serial_enable,
+	.read = buffered_serial_read,
+	.write = ti_arm_mcu_serial_write,
+	.irq_enable = ti_arm_mcu_serial_irq_enable,
+    },
+#ifdef CONFIG_GPIO
+    .gpio = {
+	.digital_write = ti_arm_mcu_gpio_digital_write,
+	.digital_read = ti_arm_mcu_gpio_digital_read,
+	.analog_write = ti_arm_mcu_gpio_analog_write,
+	.analog_read = ti_arm_mcu_gpio_analog_read,
+	.set_pin_mode = tm4c123g_set_pin_mode,
+	.set_port_val = ti_arm_mcu_gpio_set_port_val,
+	.get_port_val = ti_arm_mcu_gpio_get_port_val,
+    },
+#endif
+#ifdef CONFIG_SPI
+    .spi = {
+	.init = ti_arm_mcu_spi_init,
+	.reconf = ti_arm_mcu_spi_reconf,
+	.set_max_speed = ti_arm_mcu_spi_set_max_speed,
+	.send = ti_arm_mcu_spi_send,
+	.receive = ti_arm_mcu_spi_receive,
+    },
+#endif
+    .init = tm4c123g_init,
+    .meminfo = cortex_m_meminfo,
+    .panic = cortex_m_panic,
+    .select = ti_arm_mcu_select,
+    .get_ticks_from_boot = cortex_m_get_ticks_from_boot,
+    .get_system_clock = ti_arm_mcu_get_system_clock,
+    .msleep = ti_arm_mcu_msleep,
+};
