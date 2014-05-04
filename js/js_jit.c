6858d371abca34a156e7f37ed22fd9a8d95f7e5b
Eyal Birger <eyal.birger@gmail.com>
2014-05-04 04:39:40 +0300

    [js jit] initial ARM thumb JIT support. Still mostly useless. Calculates simple expressions

--- js/js_jit.c
+++ js/js_jit.c
@@ -23,13 +23,164 @@
  * software, even if advised of the possibility of such damage.
  */
 #include "js/js_jit.h"
+#include "js/js_obj.h"
 #include "util/tnum.h"
+#include "util/tp_types.h"
 
 static int jit_expression(scan_t *scan);
 
-#define JIT_FUNC_CALL1(func, arg1) do { \
-    tp_out(("JIT: CALL %s(%s:%d)\n", #func, #arg1, arg1)); \
+#define ARM_THUMB
+#ifdef ARM_THUMB
+
+#define ARM_THM_JIT_MAX_OPS_NUM 128
+
+static u16 jit_buffer[ARM_THM_JIT_MAX_OPS_NUM];
+static int jit_buffer_idx;
+
+static int jit_op16(u16 op)
+{
+    tp_debug(("JIT: OP 0x%x\n", op));
+
+    jit_buffer[jit_buffer_idx] = op;
+    jit_buffer_idx++;
+    if (jit_buffer_idx == ARM_THM_JIT_MAX_OPS_NUM)
+        return -1;
+
+    return 0;
+}
+
+static int jit_op32(u32 op)
+{
+    if (jit_op16(op >> 16))
+        return -1;
+
+    return jit_op16(op & 0xffff);
+}
+
+#define R0 0
+#define R1 1
+#define R2 2
+#define R3 3
+#define R4 4
+
+/* op: 0 - push, 1 - pop
+ * R: 1 - operate on lr too
+ * register_list: bitmap of registers
+ */
+#define ARM_THM_JIT_PUSH_POP(op, R, register_list) do { \
+    tp_info(("JIT: push_pop op %d, R %d, register_list %x\n", op, R, \
+        register_list)); \
+    if (jit_op16(0xb000 | ((op)<<11) | (1<<10) | ((R)<<8) | (register_list))) \
+        return -1; \
 } while(0)
+
+#define ARM_THM_JIT_PUSH(R, register_list) \
+    ARM_THM_JIT_PUSH_POP(0, R, register_list)
+#define ARM_THM_JIT_POP(R, register_list) \
+    ARM_THM_JIT_PUSH_POP(1, R, register_list)
+
+#define ARM_THM_JIT_MOV(ld, imm8) do { \
+    if (imm8 > 255) \
+        return -1; \
+    tp_info(("JIT: mov ld %d, imm8 %x\n", ld, imm8)); \
+    if (jit_op16(0x2000 | ((ld)<<8) | (imm8))) \
+        return -1; \
+} while(0)
+
+#define _ARM_THM_JIT_MOVW(i, imm4, imm3, rd, imm8) do { \
+    u32 op32; \
+    tp_info(("JIT: movw i %d, imm4 %x, imm3 %x, rd %d, imm8 %x\n", i, imm4, \
+        imm3, rd, imm8)); \
+    op32 = 0xf000 | ((i)<<10) | (1<<9) | (1<<6) | (imm4); \
+    op32 <<= 16; \
+    op32 |= ((imm3)<<12) | ((rd)<<8) | (imm8); \
+    if (jit_op32(op32)) \
+        return -1; \
+} while(0)
+
+#define _ARM_THM_JIT_MOVT(i, imm4, imm3, rd, imm8) do { \
+    u32 op32; \
+    tp_info(("JIT: movt i %d, imm4 %x, imm3 %x, rd %d, imm8 %x\n", i, imm4, \
+        imm3, rd, imm8)); \
+    op32 = 0xf000 | ((i)<<10) | (1<<9) | (1<<7) | (1<<6) | (imm4); \
+    op32 <<= 16; \
+    op32 |= ((imm3)<<12) | ((rd)<<8) | (imm8); \
+    if (jit_op32(op32)) \
+        return -1; \
+} while(0)
+
+#define ARM_THM_JIT_MOVW(rd, imm16) do { \
+    tp_info(("JIT: movw rd %d, imm16 %x\n", rd, imm16)); \
+    _ARM_THM_JIT_MOVW(((imm16) >> 11) & 1, ((imm16)>>12) & 0xf, ((imm16) >> 8) & 0x7, \
+        rd, (imm16) & 0xff); \
+} while(0) 
+
+#define ARM_THM_JIT_MOVT(rd, imm16) do { \
+    tp_info(("JIT: movt rd %d, imm16 %x\n", rd, imm16)); \
+    _ARM_THM_JIT_MOVT(((imm16) >> 11) & 1, ((imm16)>>12) & 0xf, ((imm16) >> 8) & 0x7, \
+        rd, (imm16) & 0xff); \
+} while(0)
+
+#define ARM_THM_JIT_BLX(rm) do { \
+    tp_info(("JIT: blx rm %d\n", rm)); \
+    if (jit_op16(0x4000 | (0xf<<7) | ((rm)<<3))) \
+        return -1; \
+} while(0)
+
+#define ARM_THM_JIT_REG_SET(r, val) do { \
+    tp_info(("JIT: reg set %d = %s:%x\n", r, #val, (u32)val)); \
+    ARM_THM_JIT_MOVW(r, (val) & 0xffff); \
+    ARM_THM_JIT_MOVT(r, ((val)>>16) & 0xffff); \
+} while(0)
+
+#define ARM_THM_JIT_CALL(addr) do { \
+    ARM_THM_JIT_REG_SET(R4, (u32)(addr) | 0x1); \
+    ARM_THM_JIT_BLX(R4); \
+} while(0)
+
+/* API */
+#define JIT_INIT() do { \
+    jit_buffer_idx = 0; \
+    ARM_THM_JIT_PUSH(1, (1<<R4)); \
+} while(0)
+
+#define JIT_UNINIT() do { \
+    ARM_THM_JIT_POP(1, (1<<R0)|(1<<R4)); \
+} while(0)
+
+#define JIT_FUNC_CALL1(func, arg1) do { \
+    ARM_THM_JIT_MOV(R0, arg1); \
+    ARM_THM_JIT_CALL(func); \
+    ARM_THM_JIT_PUSH(0, (1<<R0)); \
+} while(0)
+
+#define JIT_FUNC_CALL2(func, arg1) do { \
+    ARM_THM_JIT_POP(0, (1<<R2)); \
+    ARM_THM_JIT_POP(0, (1<<R1)); \
+    ARM_THM_JIT_MOV(R0, arg1); \
+    ARM_THM_JIT_CALL(func); \
+    ARM_THM_JIT_PUSH(0, (1<<R0)); \
+} while(0)
+
+static void jit_call(void)
+{
+    obj_t *(*func)(void);
+    u8 *buf;
+
+    buf = tmalloc(jit_buffer_idx * sizeof(u16), "jit_function");
+    memcpy(buf, jit_buffer, jit_buffer_idx * sizeof(u16));
+
+    /* '1' in LSB denotes thumb function call */
+    func = (obj_t *(*)(void))(buf + 1);
+    tp_out(("Result %o\n", func()));
+    tfree(buf);
+}
+
+#else
+
+#error JIT not available yet
+
+#endif
 
 static int jit_num_new(tnum_t num)
 {
@@ -87,7 +238,7 @@ static int name(scan_t *scan) \
         js_scan_next_token(scan); \
         if (lower(scan)) \
             return -1; \
-        JIT_FUNC_CALL1(obj_do_op, tok); \
+        JIT_FUNC_CALL2(obj_do_op, tok); \
         tok = CUR_TOK(scan); \
     } \
     return 0; \
@@ -104,12 +255,18 @@ static int jit_expression(scan_t *scan)
 
 int jit_statement_list(scan_t *scan)
 {
-    int rc;
+    int rc = 0;
     scan_t *scan_copy;
-
     scan_copy = js_scan_save(scan);
 
+    JIT_INIT();
+
     rc = jit_expression(scan_copy);
+
+    JIT_UNINIT();
+
+    if (!rc)
+        jit_call();
 
     js_scan_free(scan_copy);
     tp_out(("JIT Status: %s\n", rc ? "Failed" : "Success"));
