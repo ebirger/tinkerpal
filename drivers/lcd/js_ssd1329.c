be945e6d86be30f5525506fc72da34f27110796a
Eyal Birger <eyal.birger@gmail.com>
2014-06-18 17:57:55 +0300

    [ssd1329 oled] js bindings

---
+++ drivers/lcd/js_ssd1329.c
@@ -0,0 +1,37 @@
+/* Copyright (c) 2013, Eyal Birger
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * The name of the author may not be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "drivers/gpio/gpio.h"
+#include "graphics/js_canvas.h"
+#include "boards/board.h"
+#include "js/js_obj.h"
+
+int do_ssd1329_constructor(obj_t **ret, obj_t *this, int argc, obj_t *argv[])
+{
+    canvas_t *canvas;
+
+    /* TODO: allow providing pinout */
+    canvas = ssd1329_new(&board.ssd1329_params);
+    return canvas_obj_constructor(canvas, ret, this, argc, argv);
+}
