ebirger/tinkerpal

commit 7a9447e933bda832443394b539f6ac8a4f82d1c1

[js] eval: properly treat multiplicative and substraction operators - should be cast to numbers

js/js_obj.c
@@ -284,6 +284,16 @@ obj_t *obj_do_op(token_type_t op, obj_t *oa, obj_t *ob)
     case TOK_IN:
         ret = obj_do_in_op(oa, ob);
         break;
+    case TOK_MULT:
+    case TOK_DIV:
+    case TOK_MOD:
+    case TOK_MINUS:
+        {
+            obj_t *a = obj_cast(oa, NUM_CLASS), *b = obj_cast(ob, NUM_CLASS);
+
+            ret = CLASS(a)->do_op(op, a, b);
+            break;
+        }
     case TOK_NOT_EQ_STRICT:
     case TOK_IS_EQ_STRICT:
         if (CLASS(oa) != CLASS(ob))

commit 67ce8e62db1d4141999a5841aef607de29fd16ca

[js] fix memory leak introduced in 7a9447e933bda832443394b539f6ac8a4f82d1c1

js/js_obj.c
@@ -274,6 +274,17 @@ obj_t *obj_do_in_op(obj_t *oa, obj_t *ob)
     return ret;
 }
 
+static inline void obj_to_num(obj_t **o)
+{
+    obj_t *tmp = *o;
+
+    if (is_num(tmp))
+        return;
+
+    *o = obj_cast(tmp, NUM_CLASS);
+    obj_put(tmp);
+}
+
 obj_t *obj_do_op(token_type_t op, obj_t *oa, obj_t *ob)
 {
     obj_t *ret;
@@ -290,12 +301,9 @@ obj_t *obj_do_op(token_type_t op, obj_t *oa, obj_t *ob)
     case TOK_DIV:
     case TOK_MOD:
     case TOK_MINUS:
-        {
-            obj_t *a = obj_cast(oa, NUM_CLASS), *b = obj_cast(ob, NUM_CLASS);
-
-            ret = CLASS(a)->do_op(op, a, b);
-            break;
-        }
+        obj_to_num(&oa);
+        obj_to_num(&ob);
+        goto do_op;
     case TOK_NOT_EQ_STRICT:
     case TOK_IS_EQ_STRICT:
         if (CLASS(oa) != CLASS(ob))
@@ -304,6 +312,7 @@ obj_t *obj_do_op(token_type_t op, obj_t *oa, obj_t *ob)
             break;
         }
         /* Fallthrough */
+do_op:
     default:
         tp_assert(CLASS(oa)->do_op);
         ret = CLASS(oa)->do_op(op & ~STRICT, oa, ob);