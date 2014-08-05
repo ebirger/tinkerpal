2ac5f3fcc15183d9fc6fb05389eaf7b088467275
Eyal Birger <eyal.birger@gmail.com>
2014-08-05 15:00:49 +0300

    [unit tests] add unit tests to CI

--- tests/leak_check.sh
+++ tests/leak_check.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+executable="../build.Linux/tp";
+
 if [ $SKIP_TESTS ]; then
     exit 0;
 fi
 
 function lc()
 {
-    executable="../build.Linux/tp";
     valgrind -v --leak-check=full --num-callers=50 --show-reachable=yes $executable $1 &> $1.lc;
 }
+
+if [ $UNIT_TESTS ]; then
+    lc unit_test
+    grep "ERROR SUMMARY" unit_test.lc;
+    exit 0;
+fi
 
 list="closure_test.js while_test.js func_test.js exp_test.js object_test.js string_test.js prototype_test.js member_test.js for_test.js array_test.js fp_test.js self_ref.js eval_test.js func_constructor_test.js throw_test.js switch_test.js properties_test.js typed_array.js func_bind_test.js func_apply_test.js timer_test.js file_test.js arguments_test.js";
 
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+HOST_OS=`uname`;
+
+executable="../build.$HOST_OS/tp";
+
 if [ $SKIP_TESTS ]; then
     exit 0;
 fi
 
+if [ $UNIT_TESTS ]; then
+    $executable;
+    rc=$?;
+    exit $rc;
+fi
+
 list="closure_test.js while_test.js func_test.js exp_test.js object_test.js string_test.js prototype_test.js member_test.js for_test.js array_test.js fp_test.js self_ref.js eval_test.js func_constructor_test.js throw_test.js switch_test.js properties_test.js typed_array.js func_bind_test.js func_apply_test.js timer_test.js file_test.js arguments_test.js";
-
-HOST_OS=`uname`;
-
-executable="../build.$HOST_OS/tp";
 
 for l in $list; do 
 	echo "============================"
@@ -19,4 +25,3 @@ for l in $list; do
 	fi
 	echo "Test $l completed successfully";
 done
-
