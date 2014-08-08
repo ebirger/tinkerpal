476f4874ca05cf49ee0c6c54cb8ed574ddf221eb
Eyal Birger <eyal.birger@gmail.com>
2014-08-08 05:37:58 +0300

    [lcd] remove redundant include

--- drivers/lcd/js_lcd.c
+++ drivers/lcd/js_lcd.c
@@ -22,7 +22,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-#include "drivers/gpio/gpio.h"
 #include "graphics/js_canvas.h"
 #include "boards/board.h"
 #include "js/js_obj.h"
