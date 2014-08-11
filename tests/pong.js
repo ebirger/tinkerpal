2057f7723fc2d117908f2fa43933a671972108a6
Eyal Birger <eyal.birger@gmail.com>
2014-08-11 22:34:53 +0300

    [tests] enhance pong.js

--- tests/pong.js
+++ tests/pong.js
@@ -1,26 +1,33 @@
 console.log("Pong");
 
+digitalWrite(GPIO_PB2, 1);
 var l = new PCD8544();
+var l2 = new SSD1306();
 var g = new Graphics(l);
+var g2 = new Graphics(l2);
 
-function pong(x, y) {
+function pong(gr, maxx, maxy, x, y) {
     var dx = 1, dy = 1;
+    var update = compile(function() {
+        gr.circleDraw(x, y, 2, 0); 
+        x = x + dx;
+        y = y + dy;
+        gr.circleDraw(x, y, 2, 1); 
+    });
     return function() { 
-        x += dx; y += dy;
-        if (x < 5 || x > 79) dx *= -1;
-        if (y < 5 || y > 43) dy *= -1;
-        g.circleDraw(x, y, 3, 1); 
+        update();
+        if (x < 5 || x > maxx) dx *= -1;
+        if (y < 5 || y > maxy) dy *= -1;
     }
 }
 
-var ball1_step = pong(5, 5);
-var ball2_step = pong(30, 10);
-var ball3_step = pong(40, 20);
+var ball1_step = pong(g, 79, 43, 5, 5);
+var ball2_step = pong(g2, 123, 59, 5, 5);
 
 function step() {
-    l.fill(0);
     ball1_step();
     ball2_step();
-    ball3_step();
 }
-setInterval(step, 25);
+
+console.log(compile(step));
+setInterval(step, 10);
