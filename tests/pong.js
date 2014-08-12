e6da38b54ec0e11b4139d45794321b62de912bd3
Eyal Birger <eyal.birger@gmail.com>
2014-08-12 04:15:50 +0300

    [graphics] adapt pong.js sample for  double buffering

--- tests/pong.js
+++ tests/pong.js
@@ -6,23 +6,24 @@ var l2 = new SSD1306();
 var g = new Graphics(l);
 var g2 = new Graphics(l2);
 
-function pong(gr, maxx, maxy, x, y) {
+function pong(canvas, gr, maxx, maxy, x, y) {
     var dx = 1, dy = 1;
     var update = compile(function() {
-        gr.circleDraw(x, y, 2, 0); 
+        gr.circleDraw(x, y, 3, 0);
         x = x + dx;
         y = y + dy;
-        gr.circleDraw(x, y, 2, 1); 
+        gr.circleDraw(x, y, 3, 1);
+        canvas.flip();
     });
-    return function() { 
+    return function() {
         update();
         if (x < 5 || x > maxx) dx *= -1;
         if (y < 5 || y > maxy) dy *= -1;
     }
 }
 
-var ball1_step = pong(g, 79, 43, 5, 5);
-var ball2_step = pong(g2, 123, 59, 5, 5);
+var ball1_step = pong(l, g, 79, 43, 5, 5);
+var ball2_step = pong(l2, g2, 123, 59, 5, 5);
 
 function step() {
     ball1_step();
