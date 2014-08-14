9ea6ad6afa4289f0bc88042bf50969a1b42af8cb
Eyal Birger <eyal.birger@gmail.com>
2014-08-15 01:36:51 +0300

    [graphics] nicer pong.js coding

--- tests/pong.js
+++ tests/pong.js
@@ -6,13 +6,17 @@ var l2 = new SSD1306();
 var g = new Graphics(l);
 var g2 = new Graphics(l2);
 
+function ball(gr, x, y, c) {
+        gr.circleDraw(x, y, 4, c);
+}
+
 function pong(canvas, gr, maxx, maxy, x, y) {
     var dx = 1, dy = 1;
     var update = compile(function() {
-        gr.circleDraw(x, y, 3, 0);
+        ball(gr, x, y, 0);
         x = x + dx;
         y = y + dy;
-        gr.circleDraw(x, y, 3, 1);
+        ball(gr, x, y, 1);
         canvas.flip();
     });
     return function() {
@@ -30,5 +34,6 @@ function step() {
     ball2_step();
 }
 
+l2.fill(0);
 console.log(compile(step));
 setInterval(step, 10);
