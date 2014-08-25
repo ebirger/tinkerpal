cec21f3da262ba4a3978b3ff8e1c8ea25cd8817d
Eyal Birger <eyal.birger@gmail.com>
2014-08-26 01:20:51 +0300

    [stm32] usb: use different PLL value for stm32f4

--- platform/arm/stm32/stm32f4xx/system_stm32f4xx.c
+++ platform/arm/stm32/stm32f4xx/system_stm32f4xx.c
@@ -251,7 +251,7 @@
 
 /************************* PLL Parameters *************************************/
 /* PLL_VCO = (HSE_VALUE or HSI_VALUE / PLL_M) * PLL_N */
-#define PLL_M      25
+#define PLL_M      8
 /* USB OTG FS, SDIO and RNG Clock =  PLL_VCO / PLLQ */
 #define PLL_Q      7
 
