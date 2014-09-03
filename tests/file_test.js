8d2af325d61498539c90b6d40aeec5564612f8b5
Eyal Birger <eyal.birger@gmail.com>
2014-09-03 05:10:25 +0300

    [fs] tests: add fs.readdirSync() test

--- tests/file_test.js
+++ tests/file_test.js
@@ -5,3 +5,10 @@ var s2 = fs.readFileSync('Local/test.txt');
 debug.assert(s, s2);
 fs.writeFileSync('FAT/test.txt', s);
 var s2 = fs.readFileSync('FAT/test.txt');
+
+var dirs = fs.readdirSync('.');
+debug.dump(dirs);
+debug.assert(dirs[0], 'FAT');
+var files = fs.readdirSync('FAT/');
+debug.dump(files);
+debug.assert(files[0], 'TEST.TXT');
