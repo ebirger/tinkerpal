f468d71b35d327f427746f67a9386a67931406d7
Eyal Birger <eyal.birger@gmail.com>
2014-09-03 21:23:25 +0300

    [js] timers: test clearTimeout() and periodic function return

--- tests/timer_test.js
+++ tests/timer_test.js
@@ -5,3 +5,8 @@ setTimeout(function() { debug.assert(did, 1); }, 600);
 var count = 0;
 var tid = setInterval(function() { if (++count == 3) clearInterval(tid); }, 300);
 setTimeout(function() { debug.assert(count, 3); }, 1200);
+var count2 = 0;
+var tid2 = setInterval(function() { if (++count2 == 3) return -1; }, 300);
+setTimeout(function() { debug.assert(count2, 3); }, 1200);
+setTimeout(function() { }, 1600);
+setTimeout(function() {clearTimeout(); }, 1500);
