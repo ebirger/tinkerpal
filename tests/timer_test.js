f6715f62a005f66705d17839d0189565ce0a85fb
Eyal Birger <eyal.birger@gmail.com>
2014-09-03 05:57:32 +0300

    [timers] add setInterval() test

--- tests/timer_test.js
+++ tests/timer_test.js
@@ -1,3 +1,7 @@
 var did = 0;
 setTimeout(function() { console.log("Called"); did=1; }, 300);
 setTimeout(function() { debug.assert(did, 1); }, 600);
+
+var count = 0;
+var tid = setInterval(function() { if (++count == 3) clearInterval(tid); }, 300);
+setTimeout(function() { debug.assert(count, 3); }, 1200);
