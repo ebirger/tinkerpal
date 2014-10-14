917a51491fa9bff7fce29e4d3c8aedba4fb398b7
Eyal Birger <eyal.birger@gmail.com>
2014-10-14 18:25:51 +0300

    [net] rename js_etherif -> js_netif. doesn't compile yet

--- net/js_netif.c
+++ net/js_netif.c
@@ -22,7 +22,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-#include "net/js_etherif.h"
+#include "net/js_netif.h"
 #include "js/js_utils.h"
 #include "js/js_event.h"
 
