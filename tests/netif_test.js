43264cd626650f9bafa69f2e5916124016a393b8
Eyal Birger <eyal.birger@gmail.com>
2014-10-15 22:57:42 +0300

    [net] enhance netif_test a bit

--- tests/netif_test.js
+++ tests/netif_test.js
@@ -3,4 +3,11 @@ var n = new NetifINET();
 debug.assert(n.linkStatus(), true);
 debug.assert((n.MACAddrGet())[0], 0);
 debug.assert((n.MACAddrGet())[0], 0);
-n.IPConnect(function() { console.log("IP Connected: " + n.IPAddrGet()); });
+n.onPortChange(function() {
+    console.log("Link status: " + n.linkStatus());
+});
+
+n.IPConnect(function() {
+    console.log("IP Connected: " + n.IPAddrGet());
+    n.IPDisconnect();
+});
