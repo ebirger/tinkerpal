f6b05036d9359e9739c2050650fe85a1626e8dca
Eyal Birger <eyal.birger@gmail.com>
2014-10-15 23:29:28 +0300

    [net] don't test port change events as it causes the test to not exit

--- tests/netif_test.js
+++ tests/netif_test.js
@@ -3,9 +3,6 @@ var n = new NetifINET();
 debug.assert(n.linkStatus(), true);
 debug.assert((n.MACAddrGet())[0], 0);
 debug.assert((n.MACAddrGet())[0], 0);
-n.onPortChange(function() {
-    console.log("Link status: " + n.linkStatus());
-});
 
 n.IPConnect(function() {
     console.log("IP Connected: " + n.IPAddrGet());
