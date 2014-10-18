0feac7fff334eb5b9fcf06154642799a4ace4f84
Eyal Birger <eyal.birger@gmail.com>
2014-10-18 03:04:00 +0300

    [js] test different if..else if..else scenarios

--- tests/misc_test.js
+++ tests/misc_test.js
@@ -4,4 +4,38 @@ else
     good = 1;
 
 debug.assert(good, 1);
+
+good = 0;
+
+if (false)
+    debug.assert(1, 0);
+else if (1 == 0)
+    debug.assert(1, 0);
+else
+    good = 1;
+
+debug.assert(good, 1);
+
+good = 0;
+
+if (false)
+    debug.assert(1, 0);
+else if (1 == 1)
+    good = 1;
+else
+    debug.assert(1, 0);
+
+debug.assert(good, 1);
+
+good = 0;
+
+if (true)
+    good = 1;
+else if (1 == 1)
+    debug.assert(1, 0);
+else
+    debug.assert(1, 0);
+
+debug.assert(good, 1);
+
 meminfo();
