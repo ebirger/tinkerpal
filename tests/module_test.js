60f953b8481dcd5dad9e560198167b481b5aad6b
Eyal Birger <eyal.birger@gmail.com>
2014-10-18 10:27:25 +0300

    [js] enhance module test

--- tests/module_test.js
+++ tests/module_test.js
@@ -1,3 +1,15 @@
 /* Test module loading */
 as = require('assert');
-//as.equal(1, 1, "Ok");
+as2 = require('assert');
+debug.assert(as, as);
+as.equal(1, 1, "Ok");
+
+good = 0;
+try {
+    doesnt_exist = require('assert2');
+} catch(e) {
+    good = 1;
+}
+debug.assert(good, 1);
+
+therm = require('thermal_printer');
