9bb6973131f65aa2a8247f9a41f3761848a46394
Eyal Birger <eyal.birger@gmail.com>
2014-11-27 23:28:42 +0200

    Revert "[unix] use _DEFAULT_SOURCE instead of _BSD_SOURCE as the latter is deprecated"
    This reverts commit 2d41bf75f34e9ef74d94b425e5e1008321c8ff20.

--- platform/unix/sim.c
+++ platform/unix/sim.c
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #define _XOPEN_SOURCE 600 
-#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
