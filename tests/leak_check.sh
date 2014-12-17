eafdb30c2bd38ab665503cf1fdf2dca1e312464a
Eyal Birger <eyal.birger@gmail.com>
2014-12-17 20:53:38 +0200

    [ci] add JS math tests

--- tests/leak_check.sh
+++ tests/leak_check.sh
@@ -17,7 +17,7 @@ if [ $UNIT_TESTS ]; then
     exit 0;
 fi
 
-list="closure_test.js while_test.js func_test.js exp_test.js object_test.js string_test.js prototype_test.js member_test.js for_test.js array_test.js fp_test.js self_ref.js eval_test.js func_constructor_test.js throw_test.js switch_test.js properties_test.js typed_array.js func_bind_test.js func_apply_test.js timer_test.js file_test.js arguments_test.js module_test.js emit_test.js serial_test.js";
+list="closure_test.js while_test.js func_test.js exp_test.js object_test.js string_test.js prototype_test.js member_test.js for_test.js array_test.js fp_test.js self_ref.js eval_test.js func_constructor_test.js throw_test.js switch_test.js properties_test.js typed_array.js func_bind_test.js func_apply_test.js timer_test.js file_test.js arguments_test.js module_test.js emit_test.js serial_test.js math_test.js";
 
 if [[ -n $1 ]] ; then
 	lc $1;
---
+++ tests/math_test.js
@@ -0,0 +1,17 @@
+/* Most of these checks pass fine, though comparing FPs is risky */
+debug.assert(Math.sin(Math.Pi / 2), 1);
+c = Math.cos(Math.Pi / 2);
+debug.assert_cond(c > -0.005 && c < 0.005);
+debug.assert(Math.asin(1), Math.Pi / 2);
+debug.assert(Math.acos(1), 0);
+debug.assert(Math.tan(Math.Pi / 4), 1);
+debug.assert(Math.atan(0), 0);
+debug.assert(Math.log(Math.E * Math.E), 2);
+debug.assert(Math.exp(2), Math.E * Math.E);
+debug.assert(Math.floor(2.4), 2);
+debug.assert(Math.ceil(2.4), 3);
+debug.assert(Math.round(2.4), 2);
+debug.assert(Math.round(2.6), 3);
+debug.assert(Math.abs(-2), 2);
+debug.assert(Math.atan2(1, 1), Math.Pi / 4);
+debug.assert(Math.pow(10, 2), 100);
