791c4ee895cf627841037eeca88f4dd9dc8ec682
Eyal Birger <eyal.birger@gmail.com>
2015-10-19 06:28:35 +0300

    [net] remove redundant function prototype
    This function is defined and declared in net_utils.h

--- net/net_debug.h
+++ net/net_debug.h
@@ -41,7 +41,6 @@ void dhcp_msg_dump(dhcp_msg_t *msg);
 #else
 
 static inline char *eth_mac_serialize(eth_mac_t *m) { return ""; }
-static inline char *ip_addr_serialize(u32 ip) { return ""; }
 static inline void eth_hdr_dump(eth_hdr_t *hdr) { }
 static inline void arp_packet_dump(arp_packet_t *arp) { }
 static inline void ip_hdr_dump(ip_hdr_t *iph) { }
