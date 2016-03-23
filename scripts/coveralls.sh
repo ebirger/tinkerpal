98420c3f115bf616340359487facc7be35bdad38
Eyal Birger <eyal.birger@gmail.com>
2016-03-23 08:27:17 +0200

    [coverage] add codecov.io to coverage data collection

--- scripts/coveralls.sh
+++ scripts/coveralls.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-[ "$DEFCONFIG" == "unix_sim_tests_defconfig" ] && coveralls --exclude build.Linux/fs/fat/ff.c --exclude staging.Linux --exclude doc --exclude scripts --exclude tests --gcov-options '\-lp' -r . -b .
+[ "$DEFCONFIG" == "unix_sim_tests_defconfig" ] && {
+    coveralls --exclude build.Linux/fs/fat/ff.c --exclude staging.Linux --exclude doc --exclude scripts --exclude tests --gcov-options '\-lp' -r . -b .
+    bash <(curl -s https://codecov.io/bash)
+}
